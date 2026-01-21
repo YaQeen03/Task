a66de1524ecdbc10c20df0a2bfa1ef2940408311
YaQeen03 <yfaisa1323@gmail.com>
2026-01-21 04:39:39 +0300

    Add new file

--- Task#3(Section(1-3))/Task#3(Section(1-3))/obj/Release/net10.0/Task#3(Section(1-3)).AssemblyInfo.cs
+++ Task#3(Section(1-3))/Task#3(Section(1-3))/obj/Release/net10.0/Task#3(Section(1-3)).AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task#3(Section(1-3))")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78011ff73513618c87fe9ab508dff3713a6528cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task#3(Section(1-3))")]
---
+++ Task#4/File/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.Metrics;
+using System;
+using System.IO;
+
+
+namespace Task_4
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Employees.txt";
+            string[] lines = File.ReadAllLines(str);
+
+            foreach (string s in lines)
+            {
+                string[] colums = s.Split('\\');
+                string name = colums[0];
+                string salary = colums[1];
+                string department = colums[2];
+
+                int salaryI = int.Parse(salary);
+
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"Salary: {salaryI}");
+                Console.WriteLine($"Department: {department}");
+                Console.WriteLine("------------------------------");
+
+                string outputPath = "results.txt";
+                File.WriteAllText(outputPath, "\n");
+
+                foreach (string o in lines)
+                {
+                    string[] Colums = o.Split('\\');
+                    string Name = Colums[0];
+                    string Salary = Colums[1];
+                    string Department = Colums[2];
+
+                    int SalaryI = int.Parse(Salary);
+
+                    string resultLine = $"{Name} works in {Department} and earns {SalaryI}";
+                    File.AppendAllText(outputPath, resultLine + "\n");
+                }
+            }
+        }
+    }
+}
